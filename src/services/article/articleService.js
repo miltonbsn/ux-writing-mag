0942ad2b2f3408753e47fca60d5e6bd18b011932
Milton Bittencourt <miltonbst@gmail.com>
2019-07-12 01:53:54 -0300

    Upload texto 2

--- src/services/article/articleService.js
+++ src/services/article/articleService.js
@@ -13,7 +13,7 @@ const articles = [
     <p>A experiência e interação com um produto, serviço ou marca é fundamental para que uma pessoa defina sua percepção e sentimentos em relação a eles. E é isso que os profissionais de UX procuram estudar e, claro, melhorar.</p>
     <h2>O que é a Experiência do Usuário? </h2>
     <p>O termo foi cunhado pelo estadunidense Donald Norman nos anos 90, quando trabalhava como engenheiro de usabilidade para a Apple. </p>
-    <p>Quando questionado sobre o significado do termo UX (User Experience ou, em português, Experiência do Usuário), ele respondeu que são todas as etapas de contato entre uma pessoa e um produto, sistema ou serviço. Essas etapas podem causar sensações positivas ou negativas no indivíduo. Usando como exemplo um computador da Apple, a experiência do usuário começa desde o momento em que você pesquisa e se informa sobre o produto, faz a compra na loja, quando abre a caixa em sua casa, até a hora em que está usando seu Macbook no dia a dia.</p>
+    <p>Quando questionado sobre o <a href="https://www.youtube.com/embed/9BdtGjoIN4E" target="_blank"> significado do termo UX </a> (User Experience ou, em português, Experiência do Usuário), ele respondeu que são todas as etapas de contato entre uma pessoa e um produto, sistema ou serviço. Essas etapas podem causar sensações positivas ou negativas no indivíduo. Usando como exemplo um computador da Apple, a experiência do usuário começa desde o momento em que você pesquisa e se informa sobre o produto, faz a compra na loja, quando abre a caixa em sua casa, até a hora em que está usando seu Macbook no dia a dia.</p>
     <p>Don Norman defende em seu livro 'O design do dia a dia' que a experiência deve ser centrada no usuário, baseada nas suas necessidades e interesses, e 'que dê atenção especial à questão de fazer produtos compreensíveis e facilmente utilizáveis'. </p>
     <img style='width:100%;' src='https://ik.imagekit.io/uxmag/LRM_EXPORT_181064190900683_20190711_234108308_RKPNds5E3.jpeg' />
     <p>Problemas de usabilidade parecem triviais, mas podem significar a diferença entre prazer e frustração, e se estender a operações mais complexas, com vidas humanas em jogo. <p>
@@ -34,6 +34,37 @@ const articles = [
     <p>Mesmo o conceito do que é bonito pode variar muito. Leandro também já trabalhou na Booking.com que possui 198 escritórios espalhados por 70 países pelo mundo. O site é traduzido para mais de 40 idiomas e, por meio dele, são reservadas cerca de 1,5 milhão de estadias todos os dias. </p>
     <img style='width:100%;' src='https://ik.imagekit.io/uxmag/Leandro_Lima_ZgItHUVfJ.jpeg' />
     <p>O designer conta que uma das maiores preocupações da empresa é ser compreendida e funcional para todas essas pessoas 'Chineses, turcos, ocidentais, cada um tem uma percepção do que é bonito. Não podemos agradar a todos, mas queremos que todos saibam como usar nosso site'.</p>`
+  }, 
+  {
+    "id": 2, 
+    "title": "A ascensão do conteúdo na tecnologia",
+    "overview": "Quem são os UX Writers e como a profissão tem ganhado importância nos últimos anos",
+    "author": "Lilian Koyama",
+    "category": "GUIA",
+    "when": "11 de junho de 2019",
+    "coverImage": "https://ik.imagekit.io/uxmag/balao-unico-azul-com-uma-corda_53876-92777_BoRxK9SFp.jpg",
+    "body": `
+    <p>Os fundadores do site de hospedagem Airbnb, durante a concepção do projeto, tiveram que lidar com um dilema: não sabiam como passar a segurança para anfitriões abrirem suas casas e, ao mesmo tempo, os hóspedes ficarem tranquilos dormindo na propriedade de um desconhecido.</p>
+    <p>A solução? Investir na experiência do usuário do site. Não apenas em uma interface intuitiva e em um esquema de avaliação de anfitriões e hóspedes mas, principalmente, no conteúdo dos textos da página. </p>
+    <img style='width:100%;' src='https://ik.imagekit.io/uxmag/tia8378820869818833850_2ntzJWwAZ.png' />
+    <p>Na imagem acima, da página do Airbnb, a caixa de busca é representada pela lupa ao lado do texto em cinza claro Experimente "Bali", frase simples e gentil que indica a função do campo e sugere uma próxima ação. Muitos botões poluem o visual e confundem o usuário. Por isso,  para evitar distrações, as opções de filtro só aparecem quando você interage com o campo. </p>
+    <p>A construção das frases pode transformar completamente a maneira como o usuário experimenta um site: pode tanto guiá-lo às ações corretas e ter entendimento da navegação, como pode causar confusão e frustração. </p>
+    <img style='width:100%;' src='https://ik.imagekit.io/uxmag/72c9888b673ec174a39a3551ecdfc7c0_uQjTcZQkD.jpeg' />
+    <p>Assim como em um texto para jornal, um título de uma revista ou uma chamada de um anúncio em um outdoor, as palavras precisam ter uma função e um razão para ocuparem aquele espaço. No caso dos textos de UX Writing, as palavras devem instruir o usuário a engajar e ter o entendimento do produto. </p>
+    <h2>A valorização do conteúdo e surgimento dos UX Writers</h2>
+    <p>Em seu famoso relatório de tendências de Design e Tecnologia, John Maeda - considerado uma referência na área - faz uma previsão anual com bases em seus estudos e entrevistas com as principais empresas de tecnologia do mundo. Em 2017, o título de um dos artigos, traduzido para o português, é "Esqueça a programação: escrever é a nova "habilidade de unicórnio" dos designers". </p>
+    <p>"Habilidade de unicórnio" é uma referência a um termo usado como piada entre designers, já que muitos empregadores têm uma lista infinita de requisitos para uma vaga de emprego que nem sempre são características de um designer, como a programação. </p>
+    <p>Hoje, dois anos depois da previsão, empresas de tecnologia como Google, Apple, Samsung, Oracle, Facebook, todas já possuem times estruturados com UX Writers. </p>
+    <p>No Brasil o impacto da nova profissão ainda é pequeno - são poucos os profissionais de UX Writing e pouco conteúdo de apoio para quem deseja aprender. A UX Designer Cloé de Haro acredita que, assim como outras tendências já ditadas pelas grandes empresas do Vale do Silício, maior polo de tecnologia e inovação do mundo, essa irá chegar ao nosso país e ganhar um espaço cada vez maior. Segundo Cloé, os profissionais de conteúdo e redatores, que tiverem o interesse em crescer na área, terão muitas oportunidades nos próximos anos, ou mesmo meses. </p>
+    <p>Com um mercado saturado de produtos, novos serviços e páginas online, cada vez mais as empresas procuram um diferencial para criar empatia e afinidade com um usuário. Assim como o design de um produto, o conteúdo ajuda a criar uma consistência na marca, direcionar um usuário a uma próxima ação e ajudar no entendimento do seu uso. Nem sempre o design sozinho é auto-explicativo, e existem pontos que uma imagem pode ser muito mais ilustrativa e didática do que um conteúdo extenso. Combinar visual com conteúdo acaba tornando a experiência do usuário mais completa e holística. </p>
+    <h2>O outro lado do mercado de conteúdo</h2>
+    <p>A nova profissão tem atraído a atenção de vários profissionais, uma vez que o mercado de conteudistas e redatores está cada vez mais saturado e desvalorizado. </p>
+    <p>Segundo relatório sobre economia digital divulgado pela Conferência das Nações Unidas sobre Comércio e Desenvolvimento (UNCTAD, na sigla em inglês) o Brasil é o quarto país no ranking mundial de usuários de internet, com 120 milhões de pessoas conectadas. Nesse contexto, surge o Marketing Digital, produzindo informações e conteúdos para esses internautas. </p>
+    <p>Ao mesmo tempo, o jornalismo sente o impacto do crescimento da internet. Uma pesquisa da Bureau of Labor Statistics dos Estados Unidos mostra que entre 1990 e 2016, a indústria de jornal impresso encolheu quase 60%, reduzindo de 458.000 empregos para 183.000. </p>
+    <p>Em um ciclo de causa e consequência, os jornalistas acabam migrando para uma área já saturada de profissionais do Marketing Digital. </p>
+    <h2>Conclusão</h2>
+    <p>Se por um lado, o conteúdo é visto como uma mercadoria barata, por outro, é visto como um ponto estratégico e essencial para o desenvolvimento de um projeto, serviço ou produto. A profissão de UX Writer é uma maneira do profissional de conteúdo, redatores e jornalistas, aplicar suas habilidades de escrita, comunicação, pesquisa e entrevista. Mesmo que permaneçam na área do jornalismo, ter conhecimento do conceito de UX Writing pode beneficiá-los a criar produtos jornalísticos mais pensados na experiência do usuário, no caso, os leitores. </p>
+    `
   }
 ];
 
