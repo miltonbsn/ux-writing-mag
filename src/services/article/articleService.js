d655d4d8f9b48bf1f2efdfdc9f87bc8c6c4c0a9f
Milton Bittencourt <miltonbst@gmail.com>
2019-07-12 00:27:15 -0300

    Atualizando css

--- src/services/article/articleService.js
+++ src/services/article/articleService.js
@@ -6,38 +6,33 @@ const articles = [
     "category": "GUIA",
     "when": "5 junho de 2019",
     "coverImage": "https://ik.imagekit.io/uxmag/ux-mag1_vj2uMeZXS.jpg",
-    "body": 
-    "<p>Todos já ouvimos falar que a primeira impressão é a que fica. Se nas relações interpessoais a frase pode gerar discussões, na tecnologia ela é comprovadamente um fato. </p>" +
-    "<p>Segundo uma pesquisa do Marketwired de 2014, 90% dos usuários relataram parar de usar um aplicativo ou desinstalá-lo do celular porque tiveram uma primeira experiência ruim. </p>" +
-    "<p>A experiência e interação com um produto, serviço ou marca é fundamental para que uma pessoa defina sua percepção e sentimentos em relação a eles. E é isso que os profissionais de UX procuram estudar e, claro, melhorar.</p>" +
-    "<h2>O que é a Experiência do Usuário? </h2>" +
-    "<p>O termo foi cunhado pelo estadunidense Donald Norman nos anos 90, quando trabalhava como engenheiro de usabilidade para a Apple. </p>" +
-    "<p>Quando questionado sobre o significado do termo UX (User Experience ou, em português, Experiência do Usuário), ele respondeu que são todas as etapas de contato entre uma pessoa e um produto, sistema ou serviço. Essas etapas podem causar sensações positivas ou negativas no indivíduo. Usando como exemplo um computador da Apple, a experiência do usuário começa desde o momento em que você pesquisa e se informa sobre o produto, faz a compra na loja, quando abre a caixa em sua casa, até a hora em que está usando seu Macbook no dia a dia.</p>" +
-    "<p>Don Norman defende em seu livro 'O design do dia a dia' que a experiência deve ser centrada no usuário, baseada nas suas necessidades e interesses, e 'que dê atenção especial à questão de fazer produtos compreensíveis e facilmente utilizáveis'. </p>" +
-    "<img style='width:100%;' src='https://ik.imagekit.io/uxmag/LRM_EXPORT_181064190900683_20190711_234108308_RKPNds5E3.jpeg' />"+
-    "<p>Problemas de usabilidade parecem triviais, mas podem significar a diferença entre prazer e frustração, e se estender a operações mais complexas, com vidas humanas em jogo. <p>" +
-    "<p>Antes de trabalhar na Apple, em 1979, o pesquisador estava na equipe de cientistas sociais e comportamentais responsável por determinar o motivo pelo qual os operadores da sala de controle tinham cometido o acidente que quase explodiu a usina nuclear de Three Mile Island.</p>"+
-    "<p>'Para minha surpresa, concluímos que a culpa não era deles, a culpa era da concepção e desenho do projeto do controle central' conta Don Norman em seu livro.</p>" +
-    "<h2>As pessoas não gostam de pensar mais do que elas precisam</h2>" +
-    "<p>Não se ofenda com a afirmação, mas é da natureza humana evitar gastar energia à toa. E esse é um fator importante ao pensar experiência do usuário. </p>" +
-    "<p>A primeira lei de usabilidade, tanto no ambiente web quanto mobile, segundo Steve Krug, consultor especializado, é 'Não me faça pensar'. Regra que dá título ao seu livro, considerado um dos mais importantes sobre usabilidade e interação do homem com o computador.</p>" +
-    "<img style='width:100%;' src='https://ik.imagekit.io/uxmag/LRM_EXPORT_181668030856251_20190711_235112148_kccyJwVxL.jpeg' />"+
-    "<p>Se você vê uma palavra sublinhada em um texto web, por exemplo, automaticamente já sabe que ao clicar, você será direcionado a outra página referente ao assunto. Se ao invés disso, a palavra estiver apenas negritada, talvez você nunca descubra sua funcionalidade.</p>" +
-    "<p>Da mesma forma, quando alguém entra no seu site ou aplicativo buscando algo, ele deve encontrar facilmente, sem encontrar obstáculos. Isso significa que, tantos os elementos gráficos quanto o conteúdo de uma página devem ser autoexplicativos, sem necessidade de grandes esforços por parte do usuário.</p>" +
-    "<p>Tornar as páginas evidentes por si só não é apenas uma questão de se destacar entre os seus concorrentes. Dependendo da situação, as pessoas não tem escolha e acessam o seu site porque é a sua única opção. </p>" +
-    "<img style='width:100%;' src='https://ik.imagekit.io/uxmag/bu_ufsc_zJFiFoS1h.jpg' />"+
-    "<h2>Estética X Estratégia</h2>" +
-    "<p>É importante ressaltar que uma boa experiência do usuário é diferente de apenas um design bonito.</p>" +
-    "<p>Para Leandro Lima, UX Designer da empresa Klarna em Estocolmo, na Suécia, a estética faz parte da experiência porque é como a pessoa percebe um produto, mas não deve ser prioridade em um projeto 'Coisas atraentes são legais. Mas ter um problema resolvido é melhor ainda. E interfaces que de fato resolvem problemas não precisam ser atraentes ou sequer percebidas'.</p>" +
-    "<img style='width:100%;' src='https://ik.imagekit.io/uxmag/estetica_X_UX_fOk37C2fK.jpg' />"+
-    "<p>Mesmo o conceito do que é bonito pode variar muito. Leandro também já trabalhou na Booking.com que possui 198 escritórios espalhados por 70 países pelo mundo. O site é traduzido para mais de 40 idiomas e, por meio dele, são reservadas cerca de 1,5 milhão de estadias todos os dias. </p>" +
-    "<img style='width:100%;' src='https://ik.imagekit.io/uxmag/Leandro_Lima_ZgItHUVfJ.jpeg' />"+
-    "<p>O designer conta que uma das maiores preocupações da empresa é ser compreendida e funcional para todas essas pessoas 'Chineses, turcos, ocidentais, cada um tem uma percepção do que é bonito. Não podemos agradar a todos, mas queremos que todos saibam como usar nosso site'.</p>"
-
-
-
-    
- 
+    "body": `
+    <p>Todos já ouvimos falar que a primeira impressão é a que fica. Se nas relações interpessoais a frase pode gerar discussões, na tecnologia ela é comprovadamente um fato. </p>
+    <p>Segundo uma pesquisa do Marketwired de 2014, 90% dos usuários relataram parar de usar um aplicativo ou desinstalá-lo do celular porque tiveram uma primeira experiência ruim. </p>
+    <p>A experiência e interação com um produto, serviço ou marca é fundamental para que uma pessoa defina sua percepção e sentimentos em relação a eles. E é isso que os profissionais de UX procuram estudar e, claro, melhorar.</p>
+    <h2>O que é a Experiência do Usuário? </h2>
+    <p>O termo foi cunhado pelo estadunidense Donald Norman nos anos 90, quando trabalhava como engenheiro de usabilidade para a Apple. </p>
+    <p>Quando questionado sobre o significado do termo UX (User Experience ou, em português, Experiência do Usuário), ele respondeu que são todas as etapas de contato entre uma pessoa e um produto, sistema ou serviço. Essas etapas podem causar sensações positivas ou negativas no indivíduo. Usando como exemplo um computador da Apple, a experiência do usuário começa desde o momento em que você pesquisa e se informa sobre o produto, faz a compra na loja, quando abre a caixa em sua casa, até a hora em que está usando seu Macbook no dia a dia.</p>
+    <p>Don Norman defende em seu livro 'O design do dia a dia' que a experiência deve ser centrada no usuário, baseada nas suas necessidades e interesses, e 'que dê atenção especial à questão de fazer produtos compreensíveis e facilmente utilizáveis'. </p>
+    <img style='width:100%;' src='https://ik.imagekit.io/uxmag/LRM_EXPORT_181064190900683_20190711_234108308_RKPNds5E3.jpeg' />
+    <p>Problemas de usabilidade parecem triviais, mas podem significar a diferença entre prazer e frustração, e se estender a operações mais complexas, com vidas humanas em jogo. <p>
+    <p>Antes de trabalhar na Apple, em 1979, o pesquisador estava na equipe de cientistas sociais e comportamentais responsável por determinar o motivo pelo qual os operadores da sala de controle tinham cometido o acidente que quase explodiu a usina nuclear de Three Mile Island.</p>
+    <p>'Para minha surpresa, concluímos que a culpa não era deles, a culpa era da concepção e desenho do projeto do controle central' conta Don Norman em seu livro.</p>
+    <h2>As pessoas não gostam de pensar mais do que elas precisam</h2>
+    <p>Não se ofenda com a afirmação, mas é da natureza humana evitar gastar energia à toa. E esse é um fator importante ao pensar experiência do usuário. </p>
+    <p>A primeira lei de usabilidade, tanto no ambiente web quanto mobile, segundo Steve Krug, consultor especializado, é 'Não me faça pensar'. Regra que dá título ao seu livro, considerado um dos mais importantes sobre usabilidade e interação do homem com o computador.</p>
+    <img style='width:100%;' src='https://ik.imagekit.io/uxmag/LRM_EXPORT_181668030856251_20190711_235112148_kccyJwVxL.jpeg' />
+    <p>Se você vê uma palavra sublinhada em um texto web, por exemplo, automaticamente já sabe que ao clicar, você será direcionado a outra página referente ao assunto. Se ao invés disso, a palavra estiver apenas negritada, talvez você nunca descubra sua funcionalidade.</p>
+    <p>Da mesma forma, quando alguém entra no seu site ou aplicativo buscando algo, ele deve encontrar facilmente, sem encontrar obstáculos. Isso significa que, tantos os elementos gráficos quanto o conteúdo de uma página devem ser autoexplicativos, sem necessidade de grandes esforços por parte do usuário.</p>
+    <p>Tornar as páginas evidentes por si só não é apenas uma questão de se destacar entre os seus concorrentes. Dependendo da situação, as pessoas não tem escolha e acessam o seu site porque é a sua única opção. </p>
+    <img style='width:100%;' src='https://ik.imagekit.io/uxmag/bu_ufsc_zJFiFoS1h.jpg' />
+    <h2>Estética X Estratégia</h2>
+    <p>É importante ressaltar que uma boa experiência do usuário é diferente de apenas um design bonito.</p>
+    <p>Para Leandro Lima, UX Designer da empresa Klarna em Estocolmo, na Suécia, a estética faz parte da experiência porque é como a pessoa percebe um produto, mas não deve ser prioridade em um projeto 'Coisas atraentes são legais. Mas ter um problema resolvido é melhor ainda. E interfaces que de fato resolvem problemas não precisam ser atraentes ou sequer percebidas'.</p>
+    <img style='width:100%;' src='https://ik.imagekit.io/uxmag/estetica_X_UX_fOk37C2fK.jpg' />
+    <p>Mesmo o conceito do que é bonito pode variar muito. Leandro também já trabalhou na Booking.com que possui 198 escritórios espalhados por 70 países pelo mundo. O site é traduzido para mais de 40 idiomas e, por meio dele, são reservadas cerca de 1,5 milhão de estadias todos os dias. </p>
+    <img style='width:100%;' src='https://ik.imagekit.io/uxmag/Leandro_Lima_ZgItHUVfJ.jpeg' />
+    <p>O designer conta que uma das maiores preocupações da empresa é ser compreendida e funcional para todas essas pessoas 'Chineses, turcos, ocidentais, cada um tem uma percepção do que é bonito. Não podemos agradar a todos, mas queremos que todos saibam como usar nosso site'.</p>`
   }
 ];
 
