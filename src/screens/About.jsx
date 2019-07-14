27ebfafeeef930f577c06ab596b98f47747dd21b
Milton Bittencourt <miltonbst@gmail.com>
2019-07-14 01:36:53 -0300

    Página sobre

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -12,6 +12,10 @@ const useStyles = makeStyles(theme => ({
     width: "100%",
     marginBottom: 20
   },
+  speciallist: {
+    margin: 0,
+    padding: 0
+  }
 }));
 
 function About() {
@@ -22,15 +26,20 @@ function About() {
   return (
     <div className={classes.root}>
       <Container maxWidth="sm">
-        <Title>About</Title>
+        <Title>Sobre</Title>
       </Container>
 
-      <img className={classes.image} src={`http://lorempixel.com/600/400/nightlife/${Math.floor((Math.random() * 10) + 1)}`} alt="About" />
+      <img className={classes.image} src={"https://ik.imagekit.io/uxmag/Linda__YIXuI9TAi.jpeg"} alt="Sobre" />
 
       <Container maxWidth="sm">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi deserunt tenetur aliquid tempore nihil quidem maiores id nisi eos corrupti aperiam illum nesciunt, fugit enim a aliquam, minima sapiente. Hic!</p>
-        <p>Quasi molestiae atque nisi eveniet illum incidunt quisquam omnis excepturi iste. Fugiat temporibus illo error adipisci omnis voluptatibus ipsam earum, rerum magni excepturi et, debitis cumque? Rem, dolores nam. Rerum.</p>
-        <p>Assumenda repellendus vel itaque autem ratione adipisci provident optio et labore consectetur aliquam quisquam natus fugit animi similique quam enim dignissimos culpa consequuntur pariatur, quidem aliquid fugiat unde? Vero, minus.</p>
+        <p>Meu Trabalho de Conclusão de Curso da Universidade Federal de Santa Catarina foi a oportunidade ideal para desenvolver uma plataforma em que eu pudesse me dedicar ainda mais a explorar as possibilidades do UX Writing, publicar meus aprendizados e compartilhar esse conhecimento com outros jornalistas e redatores interessados. </p>
+        <p>Foi em 2017 que descobri o UX Writing, assim que o John Maeda publicou seu relatório anual de tendências, apontando a criação de conteúdo como fator essencial para uma boa Experiência do Usuário. Cursando jornalismo e trabalhando na área de tecnologia, logo me encantei pela nova profissão e decidi me aprofundar nos estudos. </p>
+        <ul className={classes.speciallist} style={{ listStyleType: "none" }}>
+        <li><strong>Textos e Gestão do Projeto:</strong> Lilian Koyama </li>
+        <li><strong>Orientação:</strong> Professora Rita Paulino </li>
+        <li><strong>Tecnologia:</strong> Milton Bittencourt </li>
+        <li><strong>Agradecimentos:</strong> Bruno Franzoni, Arthur Nascimento, Cloé de Haro e Felipe Campos </li>
+        </ul>
       </Container>
     </div>
   )
