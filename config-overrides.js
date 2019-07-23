90bcef8bc587de2d06b5f095cb474a0168072a96
Milton Bittencourt <miltonbst@gmail.com>
2019-07-23 02:17:20 -0300

    Testando opções de cache

--- config-overrides.js
+++ config-overrides.js
@@ -6,9 +6,13 @@ module.exports = function override(config, env) {
   if (env === "production") {
     console.log("Production build - Adding Workbox for PWAs");
     // Extend the default injection config with required swSrc
+    console.log(path.join(__dirname, 'src', 'custom-sw.js'));
     const workboxConfig = {
-      ...defaultInjectConfig,
-      swSrc: path.join(__dirname, 'src', 'custom-sw.js')
+  //    ...defaultInjectConfig,
+  //    swSrc: path.join(__dirname, 'src', 'custom-sw.js')
+  ...defaultInjectConfig,
+  swSrc: path.join(__dirname, "src", "custom-sw.js"),
+  importWorkboxFrom: "local" // Add this propertie
     };
     config = rewireWorkboxInject(workboxConfig)(config, env);
   }
