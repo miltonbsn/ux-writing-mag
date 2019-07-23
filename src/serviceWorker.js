7963cc2676189c104f7d5a350c354391f30676ce
Milton Bittencourt <miltonbst@gmail.com>
2019-07-23 01:39:07 -0300

    Testando opções de cache

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -34,7 +34,7 @@ export function register(config) {
 
     window.addEventListener('load', () => {
       //const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      const swUrl = `custom-sw.js`;
+      const swUrl = `/custom-sw.js`;
 
       console.log("isLocalhost", isLocalhost);
 
