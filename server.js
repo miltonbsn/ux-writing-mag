5ce836c676645517a3b70368371dd43a4b8919a4
Milton Bittencourt <miltonbst@gmail.com>
2019-07-31 16:30:40 -0300

    Corrigindo rotas

--- server.js
+++ server.js
@@ -11,4 +11,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.get('/about', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/post/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
